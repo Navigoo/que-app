9194c70ad418b4645e1119e8ddce7ef6455c580d
Navigoo <81353827+Navigoo@users.noreply.github.com>
2022-06-09 22:20:04 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import { useState, useEffect } from "react";
+import "./App.css";
 import { db } from "./firebase-config";
 import {
   collection,
@@ -10,11 +10,79 @@ import {
   doc,
 } from "firebase/firestore";
 
-
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newCourse, setNewCourse] = useState("");
+
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, course: newCourse });
+  };
+
+  const updateUser = async (id, course) => {
+    const userDoc = doc(db, "users", id);
+    const newCourse = { course: course };
+    await updateDoc(userDoc, newCourse);
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-     <p>hej</p>
+      <input
+        placeholder="Name..."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+        type="string"
+        placeholder="Course..."
+        onChange={(event) => {
+          setNewCourse(event.target.value);
+        }}
+      />
+
+      <button onClick={createUser}> Create User</button>
+      {users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              {" "}
+              Increase Age
+            </button>
+            <button
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              {" "}
+              Delete User
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDYTzFpkq6KsWiENWKaEenNJoeD454H8pU",
   authDomain: "que-app-1adc4.firebaseapp.com",
@@ -19,7 +13,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 //Create DB with firestore
-const db = getFirestore();
+
+
+export const db = getFirestore(app);
